ccdec52578eb019fc058a79efc02ef29cc9b31b9
Bernhard Wagner <gitcommit@bernhardwagner.net>
2019-06-25 16:43:38 +0200

    IMPROVES: searching for url start

--- urldecode.py
+++ urldecode.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import sys
+import re
 import urllib.parse as ul
 print()
 s = ul.unquote_plus(sys.argv[1])
 print("escaped:")
 print(s)
 print()
-i = s.index('https', 1);
+i = re.search("https?", s[1:]).start() + 1
 t = s[i:]
 print("unredirected:")
 print(t)
